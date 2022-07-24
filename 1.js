56452d3abb74b4ce899fe5d7915d03d341d59153
Rasheed <rasheedjay13@gmail.com>
2022-07-24 22:31:40 +0100

    commit

---
+++ 1.js
@@ -0,0 +1,15 @@
+const array = [1, 3, 5, 7, 11];
+let sum = 0;
+
+for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+}
+console.log(sum);
+
+const array2 = [2, 4, 6, 8];
+let sum2 = 0;
+
+for (let i = 0; i < array2.length; i++) {
+    sum2 += array2[i];
+}
+console.log(sum2);
