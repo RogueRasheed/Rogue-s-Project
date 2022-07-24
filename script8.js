5f8d95edaf2fd55965a8c8fc9003fbbb39c15b65
Rasheed <rasheedjay13@gmail.com>
2022-07-24 22:16:11 +0100

    commit

---
+++ script8.js
@@ -0,0 +1,11 @@
+const NoOfThrees = [3, 3, 36, 3, 1, 3];
+let target = 3;
+
+let counter = 0;
+for (No of NoOfThrees) {
+  if (No == target) {
+        counter++;
+    }
+};
+
+console.log(counter); 
